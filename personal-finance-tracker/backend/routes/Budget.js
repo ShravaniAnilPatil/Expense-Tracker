c187a5087d10554ebb1a46e48b8fbc13b9829349
ShravaniAnilPatil <shravanipatil1427@gmail.com>
2024-12-15 01:22:56 +0530

    budeget routes defined and imported in main app

--- personal-finance-tracker/backend/routes/Budget.js
+++ personal-finance-tracker/backend/routes/Budget.js
@@ -1,34 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const Budget = require('../models/Budget');
-const fetchBudget = require('../middleware/fetchBudget');
-
+const Budget = require('../models/Budget'); 
+const User = require('../models/User'); 
 router.post('/create', async (req, res) => {
-  try {
-    const { userId, totalAmount, startDate, endDate } = req.body;
+  const { userId, totalAmount, currentAmount, startDate, endDate } = req.body;
 
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
     const existingBudget = await Budget.findOne({ user: userId });
     if (existingBudget) {
-      return res.status(400).json({ error: 'A budget already exists for this user.' });
+      return res.status(400).json({ error: 'User already has a budget.' });
     }
-    if (new Date(startDate) >= new Date(endDate)) {
-      return res.status(400).json({ error: 'End date must be after the start date.' });
-    }
-    const newBudget = new Budget({
+    const budget = new Budget({
       user: userId,
       totalAmount,
-      currentAmount: totalAmount,
+      currentAmount,
       startDate,
       endDate,
     });
-
-    await newBudget.save();
-    res.status(201).json({ message: 'Budget created successfully!', budget: newBudget });
+    await budget.save();
+    res.status(201).json({ message: 'Budget created successfully!', budget });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error.' });
   }
 });
+  
 router.put('/update/:id', async (req, res) => {
   try {
     const { totalAmount, startDate, endDate } = req.body;
@@ -53,9 +53,10 @@ router.put('/update/:id', async (req, res) => {
     res.status(500).json({ error: 'Internal server error.' });
   }
 });
+
 router.get('/fetch/:userId', async (req, res) => {
   try {
-    const { userId } = req.params
+    const { userId } = req.params;
     const budget = await Budget.findOne({ user: userId });
     if (!budget) {
       return res.status(404).json({ error: 'Budget not found for this user.' });
