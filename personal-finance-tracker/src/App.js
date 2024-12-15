1ebb449ea2ed6ff3a864b8b1e3f05083b8535b2a
ShravaniAnilPatil <shravanipatil1427@gmail.com>
2024-12-15 11:57:19 +0530

    routes changes undone

--- personal-finance-tracker/src/App.js
+++ personal-finance-tracker/src/App.js
@@ -12,26 +12,29 @@ import Profile from './components/Home/Profile';
 import NavBar from './components/Navbar';
 import Page from './components/Page';
 import BudgetForm from './components/Home/setBudget';
+import { AuthProvider } from './components/context/AuthContext'; // Import AuthProvider
+
 function App() {
   return (
-   
     <div>
       <Router>
-      <NavBar />
-        <Routes>
-          <Route path="/signup" element={<UserSignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/addform" element={<AddForm />} />
-          <Route path="/api/expense/all/675dee04bbac4d995aab0502" element={<CheckExpense />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/NewGoal" element={<NewGoal />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/notify" element={<Notifications />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/page" element={<Page />} />
-          <Route path="/navbar" element={<NavBar/>} />
-          <Route path="/setBudget" element={<BudgetForm />} />
-        </Routes>
+        <AuthProvider> {/* Wrap entire app with AuthProvider */}
+          <NavBar />
+          <Routes>
+            <Route path="/signup" element={<UserSignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/addform" element={<AddForm />} />
+            <Route path="/api/expense/all/675dee04bbac4d995aab0502" element={<CheckExpense />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/NewGoal" element={<NewGoal />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/notify" element={<Notifications />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/page" element={<Page />} />
+            <Route path="/navbar" element={<NavBar />} />
+            <Route path="/setBudget" element={<BudgetForm />} />
+          </Routes>
+        </AuthProvider>
       </Router>
     </div>
   );
