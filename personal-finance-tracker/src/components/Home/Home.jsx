1f2983560a29ea55501df9e7a7c9512a402c81b9
ShravaniAnilPatil <shravanipatil1427@gmail.com>
2024-12-17 12:01:51 +0530

    Custom Username

--- personal-finance-tracker/src/components/Home/Home.jsx
+++ personal-finance-tracker/src/components/Home/Home.jsx
@@ -1,67 +1,83 @@
-import React,{useContext} from 'react'
-import NavBar from '../Navbar'
-import Dashboard from './Dashboard'
-import { color } from 'chart.js/helpers'
-import styles from '../../styles/home.module.css'
-import MyGoal from './Goal'
-import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
-import { useNavigate } from 'react-router-dom';
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import NavBar from "../Navbar";
+import Dashboard from "./Dashboard";
+import styles from "../../styles/home.module.css";
+import MyGoal from "./Goal";
+import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext.js";
+
 const Home = () => {
   const navigate = useNavigate();
   const { user } = useContext(AuthContext);
+  const [username, setUsername] = useState("");
+
+ 
+  useEffect(() => {
+    localStorage.setItem('userEmail', user?.email);
+    const userEmail = localStorage.getItem("userEmail"); 
+    if (userEmail) {
+      const name = userEmail.split("@")[0];
+      setUsername(name.charAt(0).toUpperCase() + name.slice(1)); 
+    } else if (user?.email) {
+      const name = user.email.split("@")[0];
+      setUsername(name.charAt(0).toUpperCase() + name.slice(1));
+    } else {
+      setUsername("User");
+    }
+  }, [user]);
+
   const handleSubmit = () => {
-    navigate('/addform');
+    navigate("/addform");
   };
 
   const handleGoal = () => {
-    navigate('/NewGoal');
+    navigate("/NewGoal");
   };
 
   return (
     <div className={styles.main}>
-        
-        <div>
-          <h1 align="center" className={styles.wel}>Welcome</h1>
-          </div>
-        <div className={styles.cardcontainer}>
-            <div className={styles.card}>
-              <h3>Set a Budget</h3>
-              <Link to="/setBudget"><button >Get Started</button></Link>
-              
-            </div>
-            
-            <div className={styles.card}>
-              <h3>Expense History</h3>
-           <Link to={`/api/expense/all/${user.id}`}>
-      <button>View Expenses</button>
-    </Link>
-              
-            </div>
+      <div>
+        <h1 align="center" className={styles.wel}>
+          Welcome {username}
+        </h1>
+      </div>
+
+      <div className={styles.cardcontainer}>
+        <div className={styles.card}>
+          <h3>Set a Budget</h3>
+          <Link to="/setBudget">
+            <button>Get Started</button>
+          </Link>
         </div>
 
-        
-        <p className={styles.hname}>Dashboard</p>
-        <Dashboard />
-        <div className={styles.btnmain}>
-          <button className={styles.btn} onClick={handleSubmit}>
-            {/* <div className={styles.circle}> <AddCircleOutlineRoundedIcon /></div> */}
-            <h6>Add an Expense</h6>
-            
-            </button>
+        <div className={styles.card}>
+          <h3>Expense History</h3>
+          <Link to={`/api/expense/all/${user?.id}`}>
+            <button>View Expenses</button>
+          </Link>
         </div>
-        <p className={styles.hname}>MyGoals</p>
-        <MyGoal />
-        <div className={styles.btnmain}>
-          <button className={styles.btn} onClick={handleGoal}>
-            {/* <div className={styles.circle}> <AddCircleOutlineRoundedIcon /></div> */}
-            <h6>Add a New Goal</h6>
-            
-            </button>
-        </div>
+      </div>
+
+      <p className={styles.hname}>Dashboard</p>
+      <Dashboard />
+
+      <div className={styles.btnmain}>
+        <button className={styles.btn} onClick={handleSubmit}>
+          <h6>Add an Expense</h6>
+        </button>
+      </div>
+
+      <p className={styles.hname}>MyGoals</p>
+      <MyGoal />
+
+      <div className={styles.btnmain}>
+        <button className={styles.btn} onClick={handleGoal}>
+          <h6>Add a New Goal</h6>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
